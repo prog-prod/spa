fc1fe9e232fae87813d6436fdf2f2097c01f57cd
andreypo <andrew.pol@mintymint.net>
2020-09-12 17:38:05 +0300

    add ga

--- resources/views/helpers/scripts.blade.php
+++ resources/views/helpers/scripts.blade.php
@@ -1,4 +1,14 @@
 @include('helpers.google-analitics')
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-177840629-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-177840629-1');
+</script>
+
 <script>
     window.default_locale = "{{ config('app.locale') }}";
     window.fallback_locale = "{{ config('app.fallback_locale') }}";
